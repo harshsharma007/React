379c216bb96a2a7bc30285ecb9ecd9c901c24e5b
Harsh Sharma <harshsharmahs@gmail.com>
2021-01-07 15:18:18 +0530

    React

--- one-react/src/App.js
+++ one-react/src/App.js
@@ -4,6 +4,35 @@ import Person from './Person/Person'
 import CustomPerson from './Person/CustomPerson'
 
 class App extends Component {
+  /*
+    state is declared as a JavaScript object. Props simply are an object giving us access to all the 
+    attributes we pass to our own components. Sometimes, you don’t want to get some information from 
+    outside but you want to have it inside a component and change it from inside there too. For example, 
+    in our App.js file, let’s say we also want to add a button which if we click it simply switches one 
+    of the names we use in the Person example.
+
+    Important: This state property is only available in components that extend components, only in 
+    class-based React components. With React 16.8, a new feature was released called React hooks with 
+    which you can also manage state in functional components. State is a reserved word.
+
+    Default way of adding a state is to add a state in class based component. You should use functional 
+    components as often as possible because you should use state with care because having state in all 
+    your components and manipulating it from anywhere in your app makes your app quickly unpredictable 
+    and hard to manage especially as it grows.
+
+    State is a special property. State can be changed and if it changes and that’s the special thing 
+    about it and it only works on that state property, if it changes it will lead React to re-render our 
+    DOM or to update the DOM.
+  */
+
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,9 +45,11 @@ class App extends Component {
           attribute, input element also have the value attribute. To make Person component more flexible,
           dynamic and configurable, it would be nice if we can pass some attributes.
         */}
-        <CustomPerson name="Max" age="28" />
-        <CustomPerson name="Manu" age="29" >My Hobbies: Racing</CustomPerson>
-        <CustomPerson name="Stephanie" age="26" />
+
+        <button>Switch Name</button>
+        <CustomPerson name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <CustomPerson name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</CustomPerson>
+        <CustomPerson name={this.state.persons[2].name} age={this.state.persons[1].age} />
       </div>
     );
   }
--- one-react/src/Person/CustomPerson.js
+++ one-react/src/Person/CustomPerson.js
@@ -9,24 +9,30 @@ const customPerson = (props) => {
     return (
         <div>
             <p>I'm {props.name} and I am {props.age} years old!</p>
+
             {/*
                 To fetch "My Hobbies: Racing" section we need to write the below code. "children" is the
                 reserved word. We didn't pass anything as children on our persons, we only pass name and
                 age.
+
                 Children refers to any elements and this includes plain text as we have it here between
                 the opening and closing tag of our component, and you could nest complex html code
                 in-between too.
+                
                 This doesn't have to be just text, could be unordered list with multiple list items,
                 could be other react components, anything can go between here. And these children are now
                 output with this syntax.
+                
                 If we inspect the output, we see that an empty paragraph is rendered for every "CustomPerson"
                 that we are calling from App.js. So the paragraph is there but it's empty because
                 props.children is basically undefined as null because we have nothing between opening
                 and closing tag.
+                
                 This is important to know you can put your content into your component from outside, not
                 only by passing props but if you want to pass some structured html content also by placing
                 it between the opening and closing tag and accessing it with props.children.
             */}
+            
             <p>{props.children}</p>
         </div>
     )
