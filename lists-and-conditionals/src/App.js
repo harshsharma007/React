6253e5d648c85356b4b7f43088e48e339b52a9db
Harsh Sharma <harshsharmahs@gmail.com>
2021-01-16 20:13:04 +0530

    Lists

--- lists-and-conditionals/src/App.js
+++ lists-and-conditionals/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
 import CustomPerson from './Person/CustomPerson'
 
 class App extends Component {
@@ -38,7 +37,15 @@ class App extends Component {
   */
 
   togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({ showPersons: !doesShow })
 
+    /*
+      Important: setState code gets merged with the other state. setState with an object where we only
+      set showPersons does not mean that the entire state gets replaced with showPersons only, the old
+      state persons, another state simply is not touched. React merges the updated showPersons value
+      for us into whole state.
+    */
   }
 
   render() {
@@ -54,14 +61,46 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button style={style} onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
-        <div>
-          <CustomPerson name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <CustomPerson name={this.state.persons[1].name} age={this.state.persons[1].age}
-            click={this.togglePersonsHandler} changed={this.nameChangedHandler}>
-            My Hobbies: Racing</CustomPerson>
-          <CustomPerson name={this.state.persons[2].name} age={this.state.persons[2].age} />
-        </div>
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+
+        {/*
+          Rendering Content Conditionally
+          The below code is a JavaScript code. It's not a html code. Hence we can work with it as we can
+          work with JavaScript code. If we want to render this dynamically we can enclose it in single
+          quotation mark, the whole div wrapping the persons. Now, between the quotation marks you learned,
+          you can write JavaScript expressions to render content conditionally by adding a ternary 
+          expression. You can not add if condition but you can add simple statements, no block statements
+          as part of this dynamic syntax.
+
+          We can add a ternary expression which is a default JavaScript construct checks this condition
+          by adding a question mark and then we decide what to do if this is true.
+        */}
+
+        {
+          this.state.showPersons === true ?
+            <div>
+              <CustomPerson name={this.state.persons[0].name} age={this.state.persons[0].age} />
+              <CustomPerson name={this.state.persons[1].name} age={this.state.persons[1].age}
+                changed={this.nameChangedHandler}>
+                My Hobbies: Racing</CustomPerson>
+              <CustomPerson name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+            : null
+        }
+
+        {/*
+          Rendering Content Conditionally
+          The syntax may be confusing but what we will do in the end here is simply take advantage of
+          the fact that all we write here, in the end is JavaScript. We can interject JavaScript
+          expressions into JSX with single curly braces. This means we can access any JavaScript
+          property and use it in a ternary expression. This is a default JavaScript construct. A ternary
+          expression is created by having some condition, this doesn't have to be a property, it could
+          also be a check where we see if that is true.
+
+          If the condition is true, we are calling React.createElement. We are not calling it explicitly
+          but the code we wrote is converted to it behind the scenes. we also have an else condition
+          separated by a colon.
+        */}
       </div>
     );
   }
