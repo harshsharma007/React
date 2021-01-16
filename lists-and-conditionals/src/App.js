9ff9af10b87002688dc198574ae716f6b99f2414
Harsh Sharma <harshsharmahs@gmail.com>
2021-01-16 22:59:53 +0530

    Lists

--- lists-and-conditionals/src/App.js
+++ lists-and-conditionals/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import customPerson from './Person/CustomPerson';
 import CustomPerson from './Person/CustomPerson'
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'abc', name: 'Max', age: 28 },
+      { id: 'bcd', name: 'Manu', age: 29 },
+      { id: 'cde', name: 'Stephanie', age: 26 }
     ],
     otherState: 'Some other value',
     showPersons: false
@@ -47,6 +46,13 @@ class App extends Component {
       state persons, another state simply is not touched. React merges the updated showPersons value
       for us into whole state.
     */
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   }
 
   render() {
@@ -92,11 +98,56 @@ class App extends Component {
             persons. We want to convert this into something else. The map method which we are using here
             conveniently simply returns a new array. Our goal simply is to return JSX here so that every
             element of the person's array get mapped into a JSX representative of it.
+
+            Lists & State
+            When I click the person I want to execute this method and I want to delete this person. For
+            that, we need to know which person is this. For this, map method also exposes a second
+            argument, index in the array. If you use more than one argument in an ES6 arrow function,
+            you have to wrap the arguments in parentheses.
+
+            Updating State Immutably
+            The flaw of this approach is that in JavaScript, objects and arrays are reference types,
+            when I get persons from my state I actually get a pointer to the original person's object
+            managed by React, to the original state. If I then splice it here, I already mutate this
+            original data and whilst it does work without throwing an error, this is not really how
+            you should do it, this can lead to unpredictable apps and is definitely a bad practice.
+
+            A good practice is to create a copy of your persons array before manipulating it and a simple
+            way of doing this is by calling the slice method. Slice without arguments simply copies
+            the full array and returns a new one which is then stored here. An alternative to this
+            approach would be to use an ES6 feature, the spread operator. This appraoch is the better 
+            approach because you should always update state in an immutable fashion, so without mutating 
+            the original state first.
+
+            Lists & Keys
+            The key prop is an important property we should add when rendering lists of data and that
+            is why react tells us to do so. We haven't specified a key property in person. It's a
+            default property react expects to find on an element no matter if it's a custom component
+            or a default html element which you render through a list, basically by mapping an array
+            into JSX elements. This key property helps react update the list efficiently.
+
+            Consider the case we have here that we delete elements from the list, of course it works.
+            But behind the scenes react needs to find out what it actually needs to adjust in the
+            overall DOM. It has a Virtual DOM where it compares what it would render now if it were to
+            call the render method now or if it did actually adjust the real DOM with the result of the
+            render method to the previous DOM it rendered. It does this compression of the future with
+            the past basically. And for lists, it of course needs to find out which elements changed 
+            and react isn't human, it doesn't clearly see as we do that we have three distinct elements
+            with different names being rendered.
+
+            So it doesn't see that this is Max, Manu and Stephanie, to react, it's just has a bunch of
+            person components and it doesn't deeply inspect them that would be super inefficient. So
+            by default it will just re-render the whole list and for a long list this is very inefficient.
+            That is why we should assign a key property to allow react to keep track of the individual
+            elements so that it has a clear property it can compare between the different elements to
+            find out which elements changed and which didn't. So that it only re-renders the elements
+            which did change and not the whole list.
           */}
 
           {
-            this.state.persons.map(person => {
-              return <CustomPerson name={person.name} age={person.age} />
+            this.state.persons.map((person, index) => {
+              return <CustomPerson click={() => this.deletePersonHandler(index)}
+                name={person.name} age={person.age} key={person.id} />
             })
           }
         </div>
@@ -125,11 +176,11 @@ class App extends Component {
         {
           this.state.showPersons === true ?
             <div>
-              <CustomPerson name={this.state.persons[0].name} age={this.state.persons[0].age} />
+              {/* <CustomPerson name={this.state.persons[0].name} age={this.state.persons[0].age} />
               <CustomPerson name={this.state.persons[1].name} age={this.state.persons[1].age}
                 changed={this.nameChangedHandler}>
                 My Hobbies: Racing</CustomPerson>
-              <CustomPerson name={this.state.persons[2].name} age={this.state.persons[2].age} />
+              <CustomPerson name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
               {persons}
             </div>
             : null
